ac62caeba29a5e9d7adca31d9bf8d409637ab747
DanielaE1512 <d.echavarria@uniandes.edu.co>
2024-04-14 13:02:02 -0500

    Agregado

---
+++ Scripts/data.js
@@ -0,0 +1,7 @@
+import { Serie } from './Serie.js';
+export var series = [
+    new Serie(1, "Bridgerton", "Netflix", 2, "Los Bridgerton es una serie que cuenta desde una perspectiva feminista una historia de amor durante el Período de Regencia en el Reino Unido.", "https://cdn.forbes.com.mx/2021/02/BRIDGERTON-Netflix-serie.jpg"),
+    new Serie(2, "Game of Thrones", "HBO", 7, "American fantasy drama", "https://www.latercera.com/resizer/8KOI_77flZtzCInR2RGRkGeyBaI=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/U5G7QUP6GZDITGBPMQ23V52SGA.jpg"),
+    new Serie(3, "Sherlock", "BBC", 4, "Sherlock depicts consulting detective Sherlock Holmes (Benedict Cumberbatch) solving various mysteries in modern-day London. Holmes is assisted by his flatmate and friend, Dr John Watson (Martin Freeman), who has returned from military service in Afghanistan with the Royal Army Medical Corps", "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2023/09/actores-sherlock-holmes-3133786.jpg"),
+    new Serie(4, "A Very English Scandal", "BBC", 2, "A Very English Scandal is a fact-based three-part British television comedy-drama miniseries based on John Preston's book of the same name.", "https://i.ytimg.com/vi/pj8MTWVrl9s/maxresdefault.jpg"),
+];
---
+++ data.ts
@@ -0,0 +1,15 @@
+import { Serie } from './Serie';
+
+export const series: Serie[] = [
+    new Serie(1, "Bridgerton", "Netflix", 2, "Los Bridgerton es una serie que cuenta desde una perspectiva feminista una historia de amor durante el Período de Regencia en el Reino Unido.", "https://cdn.forbes.com.mx/2021/02/BRIDGERTON-Netflix-serie.jpg"),
+    
+    new Serie (2, "Game of Thrones","HBO", 7, "American fantasy drama",
+    "https://i.imgur.com/TDCEV1S.jpg"),
+    
+    new Serie (3, "Sherlock", "BBC",  4, "Sherlock depicts consulting detective Sherlock Holmes (Benedict Cumberbatch) solving various mysteries in modern-day London. Holmes is assisted by his flatmate and friend, Dr John Watson (Martin Freeman), who has returned from military service in Afghanistan with the Royal Army Medical Corps",
+     "https://i.imgur.com/02B7qhj.jpg"),
+    
+    new Serie (4, "A Very English Scandal", "BBC", 2, "A Very English Scandal is a fact-based three-part British television comedy-drama miniseries based on John Preston's book of the same name.",
+     "https://i.imgur.com/D4y3DrQ.jpg"),
+];
+
